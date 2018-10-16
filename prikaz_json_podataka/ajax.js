d00214020321387995065dab27cd8c99b140a32b
Aleksandar <aleksandarrlm@gmail.com>
2018-10-16 22:02:49 +0200

    Initial commit

---
+++ prikaz_json_podataka/ajax.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    var kola = ['bmw','audi','jugo'];
+
+   var json = JSON.stringify(kola);
+
+   var obj = JSON.parse('{}');
+
+
+  //  console.log(obj);
+});
---
+++ rezervacija_gostiju/server.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sale
+ * Date: 4/1/2018
+ * Time: 11:20 PM
+ */
+
+class server
+{
+
+}
